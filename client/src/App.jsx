22a3e03752a0f7dd78fe8395d49f9dd5fd71943b
akramkaid1 <k.akramengineer@gmail.com>
2024-04-26 18:04:54 +0100

    akram

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import TaskesBar from "./componenet/taskesBar.jsx"
 import pics from "./assets/hour.svg"
 import SearchBar from "./componenet/searchBar.jsx"
 import HomePage from "./HomePage/homePage.jsx"
+import Card from "./componenet/Card.jsx"
+import Cards from "./componenet/Cards.jsx"
 function App() {
 
  return (
     <div>
-    <HomePage/>
+    <Cards/>
     </div>
   )
 }
