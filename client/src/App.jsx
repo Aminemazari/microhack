3e061137025f633256fe5530a3131bf7e47c4a7c
Aminemazari <mazariamine095@gmail.com>
2024-04-26 06:49:19 +0100

    testing

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { ScheduleComponent } from "@syncfusion/ej2-react-schedule"
+function App() {
+
+ return (
+    <>
+     <ScheduleComponent/>
+    </>
+  )
+}
+
+export default App
